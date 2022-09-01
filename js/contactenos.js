085978b7d15611101566d63b49997b7d696968bb
Cesar Augusto Diaz Arriaga <black@utp.edu.co>
2022-08-31 22:38:15 -0500

    Primera clase de JS

---
+++ js/contactenos.js
@@ -0,0 +1,36 @@
+function mostrarMensaje(mensaje) {
+    alert(mensaje);
+}
+
+function incrementarContador() {
+    let valor = document.getElementById("contador").value;
+
+    /*
+     console.log("El valor obtenido es: ", valor);
+
+    if (valor > 5) {
+        alert("El numero es mayor a 5");
+    } else if (valor >= 0) {
+        alert("El numero es positivo");
+    } else {
+        alert("El numero es negativo");
+    }
+
+    for(i=0; i < 5; i++) {
+        console.log(i);
+    }
+ */
+    valor++;
+    document.getElementById("contador").value = valor;
+
+    let modificador = 'b';
+    let color = 'black';
+    if(valor % 5 == 0) {
+        color = "yellow";
+    }
+    if(valor % 3 == 0) {
+        modificador = 'i';
+    }
+    document.getElementById("salida").innerHTML = '<'+modificador+'>'+valor+"</"+modificador+">";
+    document.getElementById("salida").style.color = color;
+}
